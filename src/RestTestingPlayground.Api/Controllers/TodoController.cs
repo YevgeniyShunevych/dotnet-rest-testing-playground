c20615c3d85cb201f4e3b01ea77a1bd3e8d2c07b
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2022-01-12 15:59:28 +0200

    Add initial API project

---
+++ src/RestTestingPlayground.Api/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestTestingPlayground.Api.Models;
+
+namespace RestTestingPlayground.Api.Controllers
+{
+    [ApiController]
+    [Route("todos")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public TodoItem Get(int id)
+        {
+            return new TodoItem
+            {
+                Id = id,
+                UserId = 1,
+                Title = "delectus aut autem",
+                Completed = false
+            };
+        }
+    }
+}
---
+++ src/RestTestingPlayground.Api/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace RestTestingPlayground.Api.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
