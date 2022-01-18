209af8123c9a55d6bdcd2667df396ba601c7055b
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2022-01-18 16:40:34 +0200

    Add `UsingTestServerWithWireMockAndConfigurationParameterTests`

---
+++ src/RestTestingPlayground.Api/Controllers/ExternalViaConfigurationParameterTodoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using RestTestingPlayground.Api.Models;
+
+namespace RestTestingPlayground.Api.Controllers
+{
+    [ApiController]
+    [Route("externalviaconftodos")]
+    public class ExternalViaConfigurationParameterTodoController
+    {
+        private readonly IConfiguration _configuration;
+
+        public ExternalViaConfigurationParameterTodoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TodoItem> Get(int id)
+        {
+            string externalUrl = _configuration.GetValue<string>("ExternalServiceUrl");
+
+            RestClient restClient = new RestClient(externalUrl);
+
+            return await restClient.GetAsync<TodoItem>(new RestRequest($"/todos/{id}"));
+        }
+    }
+}
---
+++ test/RestTestingPlayground.Tests/UsingTestServerWithWireMockAndConfigurationParameterTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using RestSharp;
+using RestTestingPlayground.Api;
+using RestTestingPlayground.Api.Models;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace RestTestingPlayground.Tests
+{
+    [TestFixture]
+    public class UsingTestServerWithWireMockAndConfigurationParameterTests
+    {
+        private WireMockServer _externalServiceMock;
+
+        private TestServer _testServer;
+
+        private HttpClient _httpClient;
+
+        private RestClient _restClient;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _externalServiceMock = WireMockServer.Start();
+
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration(configurationBuilder =>
+                {
+                    configurationBuilder.AddInMemoryCollection(
+                        new Dictionary<string, string>
+                        {
+                            ["ExternalServiceUrl"] = _externalServiceMock.Urls[0]
+                        });
+                });
+
+            _testServer = new TestServer(webHostBuilder);
+
+            _httpClient = _testServer.CreateClient();
+            _restClient = new RestClient(_httpClient, new RestClientOptions(_httpClient.BaseAddress));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _externalServiceMock.Dispose();
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [Test]
+        public async Task Get()
+        {
+            var setupItem = new TodoItem
+            {
+                Id = 3,
+                UserId = 9,
+                Title = "Some title",
+                Completed = true
+            };
+
+            _externalServiceMock
+                .Given(Request.Create().WithPath("/todos/1").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithBodyAsJson(setupItem));
+
+            var response = await _restClient.ExecuteGetAsync<TodoItem>(new RestRequest("/externalviaconftodos/1"));
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Data.Should().BeEquivalentTo(setupItem);
+        }
+    }
+}
