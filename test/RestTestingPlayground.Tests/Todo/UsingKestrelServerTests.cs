6c98ffcd53e5703e85536d8c7550321b41fbbf99
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2022-01-18 12:57:33 +0200

    Add `UsingKestrelServerTests`

---
+++ test/RestTestingPlayground.Tests/Todo/UsingKestrelServerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using NUnit.Framework;
+using RestSharp;
+using RestTestingPlayground.Api;
+using RestTestingPlayground.Api.Models;
+
+namespace RestTestingPlayground.Tests.Todo
+{
+    public class UsingKestrelServerTests
+    {
+        private IWebHost _webHost;
+
+        private HttpClient _httpClient;
+
+        private RestClient _restClient;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _webHost = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .Build();
+
+            _webHost.StartAsync();
+
+            var webHostUrls = _webHost.ServerFeatures
+                .Get<IServerAddressesFeature>()
+                .Addresses;
+
+            var testUrl = webHostUrls.First();
+
+            _httpClient = new HttpClient { BaseAddress = new Uri(testUrl) };
+
+            _restClient = new RestClient(_httpClient, new RestClientOptions(_httpClient.BaseAddress));
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            _httpClient.Dispose();
+
+            await _webHost.StopAsync();
+            _webHost.Dispose();
+        }
+
+        [Test]
+        public async Task Get()
+        {
+            var response = await _restClient.ExecuteGetAsync<TodoItem>(new RestRequest("/todos/1"));
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Data.Should().BeEquivalentTo(
+                new TodoItem
+                {
+                    Id = 1,
+                    UserId = 1,
+                    Title = "Test",
+                    Completed = false
+                });
+        }
+    }
+}
